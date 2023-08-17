c9122e9192c72ed009d6cd6cdc04d793614d26c6
Martin Friščić <martin.friscic6@gmail.com>
2023-08-17 18:57:05 +0200

    [BACKEND] add login logic

--- backend/pc-hut/app/Http/Controllers/AuthController.php
+++ backend/pc-hut/app/Http/Controllers/AuthController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Validation\Rules\Password;
 
 class AuthController extends Controller
@@ -35,5 +36,35 @@ class AuthController extends Controller
             'user' => $user,
             'token' => $token,
         ]);
+    }
+
+    public function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => 'required|email|string|exists:user,email',
+            'password' => [
+                'required',
+            ],
+            'remember' => 'boolean'
+        ]);
+
+        $remember = $credentials['remember'] ?? false;
+        unset($credentials['remember']);
+
+        if (!Auth::attempt($credentials, $remember)) {
+
+            return response([
+                'error' => 'Ovaj korisnik ne postoji'
+            ], 422);
+
+            $user = Auth::user();
+            $token = $user->createToken('main')->plainTextToken;
+
+            return response([
+                'user' => $user,
+                'token' => $token,
+            ]);
+        }
     }
 }
