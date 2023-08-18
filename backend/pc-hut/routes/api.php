bdab220c0f469c952fe6107375ac041bc572f00f
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-18 13:32:52 +0200

    [BACKEND] add switch type controller, model, get endpoints

--- backend/pc-hut/routes/api.php
+++ backend/pc-hut/routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\Cooling_type_controller;
+use App\Http\Controllers\KeyboardController;
 use App\Http\Controllers\Part_controller;
+use App\Http\Controllers\SwitchTypeController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,3 +16,6 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::get('cooling_types', [Cooling_type_controller::class, 'getAll']);
 Route::get('parts', [Part_controller::class, 'getAll']);
+
+Route::get('switch-types', [SwitchTypeController::class, 'index']);
+Route::get('switch-types/{id}', [SwitchTypeController::class, 'show']);
