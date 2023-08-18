29c367aed615d837581d67f0368db12d245f6609
Martin Friščić <56409530+Fr1k1@users.noreply.github.com>
2023-08-18 15:08:38 +0200

    Frontend components (#3)
    * [FRONTEND] Add navbar component
    
    * [FRONTEND] Add new logo
    
    * [FRONTEND] fix height
    
    ---------
    
    Co-authored-by: Ana Skarica <ana.skarica.as@gmail.com>

--- frontend/vue/src/main.js
+++ frontend/vue/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import store from "./store";
 import App from "./App.vue";
 import router from "./router";
+import "./style.scss";
 
 createApp(App).use(store).use(router).mount("#app");
--- frontend/vue/src/router/index.js
+++ frontend/vue/src/router/index.js
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 import LoginPage from "../views/LoginPage.vue";
 import RegisterPage from "../views/RegisterPage.vue";
+import Navbar from "../components/Navbar/Navbar.vue";
 
 const routes = [
+  {
+    path: "/",
+    name: "HomePage",
+    component: Navbar,
+  },
   {
     path: "/login",
     name: "Login",
