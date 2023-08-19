c86ee1dcd4af3fe11b37729804631d0091de8f29
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-19 11:22:03 +0200

    [FRONTEND] add auth layout

--- frontend/vue/src/views/LoginPage.vue
+++ frontend/vue/src/views/LoginPage.vue
@@ -1,11 +1,7 @@
 <template>
-  <div class="login-wrapper">
-    <img class="img-1" src="../assets/cpu.png" alt="cpu" />
-    <img class="img-2" src="../assets/gaming.png" alt="gaming" />
-    <img class="img-3" src="../assets/gamingpc.png" alt="gamingpc" />
-    <img class="img-4" src="../assets/graphics-card.png" alt="graphics" />
+  <div class="login-outer">
     <div class="login-inner">
-      <h2>Login</h2>
+      <h2>Prijava</h2>
       <form @submit="login" id="login">
         <InputField
           placeholder="Email *"
@@ -54,43 +50,11 @@ function login(e) {
 
 <style lang="scss">
 @import "../utils/theme.scss";
-
-.login-wrapper {
-  overflow: hidden;
-  background: linear-gradient(135deg, $colorBgPrimary, $colorPrimary);
+.login-outer {
   width: 100%;
-  height: 100vh;
   display: flex;
   align-items: center;
-  justify-content: center;
   flex-direction: column;
-  position: relative;
-
-  img {
-    position: absolute;
-
-    &.img-1 {
-      top: 0;
-      left: 25px;
-      rotate: -20deg;
-      width: 50%;
-    }
-
-    &.img-2 {
-      display: none;
-    }
-
-    &.img-3 {
-      bottom: -20px;
-      right: -100px;
-      rotate: 10deg;
-      width: 70%;
-    }
-
-    &.img-4 {
-      display: none;
-    }
-  }
 
   .login-inner {
     z-index: 1;
@@ -117,57 +81,6 @@ function login(e) {
       align-items: center;
       flex-direction: column;
       gap: 24px;
-    }
-  }
-}
-
-@media screen and ($tablet) {
-  .login-wrapper {
-    img {
-      &.img-1 {
-        right: 25%;
-        width: 300px;
-      }
-
-      &.img-3 {
-        width: 300px;
-      }
-    }
-  }
-}
-
-@media screen and ($desktop) {
-  .login-wrapper {
-    img {
-      &.img-1 {
-        top: 0;
-        right: 25%;
-        left: auto;
-        width: 360px;
-      }
-
-      &.img-2 {
-        display: block;
-        bottom: -100px;
-        left: 20%;
-        rotate: -25deg;
-        width: 320px;
-      }
-
-      &.img-3 {
-        bottom: -20px;
-        right: -100px;
-        rotate: 10deg;
-        width: 340px;
-      }
-
-      &.img-4 {
-        display: block;
-        top: 80px;
-        left: 3%;
-        rotate: 25deg;
-        width: 270px;
-      }
     }
   }
 }
