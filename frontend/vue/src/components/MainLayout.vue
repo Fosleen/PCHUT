5fc11acd97f508f1726eefa8400339ddb0da2ab0
Martin Friščić <martin.friscic6@gmail.com>
2023-08-22 14:50:24 +0200

    [FRONTEND] More main layout improvements

--- frontend/vue/src/components/MainLayout.vue
+++ frontend/vue/src/components/MainLayout.vue
@@ -25,19 +25,14 @@ import Footer from "./Footer.vue";
 }
 
 .dashboard-layout-wrapper {
-  width: 100vw;
   height: 100vh;
   max-width: 100vw;
   max-height: 100vh;
-  /* display: grid;
-  grid-template-columns: auto 1fr; */
 }
 
 .main-section {
   display: flex;
   flex-direction: column;
   max-height: 1fr;
-  overflow-y: scroll;
-  /* padding: 24px; */
 }
 </style>
