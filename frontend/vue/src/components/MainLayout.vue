9ae0b2b698568a8f5a227b66e95a96a1685ef2fc
Martin Friščić <martin.friscic6@gmail.com>
2023-08-22 14:26:07 +0200

    [FRONTEND] Fix main layout

--- frontend/vue/src/components/MainLayout.vue
+++ frontend/vue/src/components/MainLayout.vue
@@ -1,8 +1,13 @@
 <template>
-  <div>
+  <div class="dashboard-layout-wrapper">
     <Navbar />
-    <router-view></router-view>
-    <Footer />
+
+    <div class="main-section">
+      <div class="content-wrapper">
+        <router-view class="content-wrapper"></router-view>
+      </div>
+      <Footer />
+    </div>
   </div>
 </template>
 
@@ -11,4 +16,28 @@ import Navbar from "./Navbar.vue";
 import Footer from "./Footer.vue";
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.content-wrapper {
+  flex-grow: 1;
+  width: 100%;
+  position: relative;
+  background-color: red;
+}
+
+.dashboard-layout-wrapper {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100vw;
+  max-height: 100vh;
+  /* display: grid;
+  grid-template-columns: auto 1fr; */
+}
+
+.main-section {
+  display: flex;
+  flex-direction: column;
+  max-height: 1fr;
+  overflow-y: scroll;
+  /* padding: 24px; */
+}
+</style>
