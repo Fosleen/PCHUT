1c934fa5e38850151a575cd496c68f5fa7bc5bb7
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-22 14:30:51 +0200

    [FRONTEND] add hero and slider

--- frontend/vue/src/views/Homepage.vue
+++ frontend/vue/src/views/Homepage.vue
@@ -1,9 +1,26 @@
 <template>
   <div>
+    <Hero />
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1>
     <h1>Početna stranica</h1>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Hero from "../components/Hero.vue";
+</script>
 
 <style lang="scss" scoped></style>
