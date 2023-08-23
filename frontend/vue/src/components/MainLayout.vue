acd05b1ae30522fdb5db53e7144474af170184fb
Martin Friščić <martin.friscic6@gmail.com>
2023-08-23 12:07:19 +0200

    [FRONTEND] Layout improvements

--- frontend/vue/src/components/MainLayout.vue
+++ frontend/vue/src/components/MainLayout.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="dashboard-layout-wrapper">
-    <Navbar />
-
     <div class="main-section">
       <div class="content-wrapper">
+        <Navbar />
+
         <router-view></router-view>
+        <Footer />
       </div>
-      <Footer />
     </div>
   </div>
 </template>
@@ -21,13 +21,21 @@ import Footer from "./Footer.vue";
   flex-grow: 1;
   width: 100%;
   position: relative;
-  background-color: red;
+  //background-color: red;
 }
 
 .dashboard-layout-wrapper {
+  width: 100vw;
   height: 100vh;
   max-width: 100vw;
   max-height: 100vh;
+  display: grid;
+  grid-template-columns: 1fr;
+  overflow-x: hidden;
+
+  /* @media screen and (min-width: 1440px) {
+    grid-template-columns: auto 1fr;
+  } */
 }
 
 .main-section {
--- frontend/vue/src/views/Homepage.vue
+++ frontend/vue/src/views/Homepage.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <Hero />
+    <PCPartCard />
+    <!-- <h1>Početna stranica</h1>
     <h1>Početna stranica</h1>
     <h1>Početna stranica</h1>
     <h1>Početna stranica</h1>
@@ -27,13 +28,14 @@
     <h1>Početna stranica</h1>
     <h1>Početna stranica</h1>
     <h1>Početna stranica</h1>
-    <h1>Početna stranica</h1>
-    <h1>Početna stranica</h1>
+    <h1>Početna stranica</h1> -->
   </div>
 </template>
 
 <script setup>
-import Hero from "../components/Hero.vue";
+// import Hero from "../components/Hero.vue";
+import PCBuilderComponentCard from "../components/PCBuilderComponentCard.vue";
+import PCPartCard from "../components/PCPartCard.vue";
 </script>
 
 <style lang="scss" scoped></style>
