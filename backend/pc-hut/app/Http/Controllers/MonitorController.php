5b125ac11196bedf4e8a4e0519699a0170f4f105
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-25 16:24:16 +0200

    [BACKEND] fix monitor get and post methods

--- backend/pc-hut/app/Http/Controllers/MonitorController.php
+++ backend/pc-hut/app/Http/Controllers/MonitorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Component;
 use App\Models\Monitor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
@@ -11,7 +12,7 @@ class MonitorController extends Controller
 {
     public function getAll()
     {
-        $monitors = Monitor::all();
+        $monitors = Monitor::with('component')->get();
 
         if ($monitors->count() > 0) {
             $data = [
@@ -21,10 +22,10 @@ class MonitorController extends Controller
 
             return response()->json($data, 200);
         } else {
-            return response()->json(
-                ['status' => 404, 'monitors' => "No response"],
-                404
-            );
+            return response()->json([
+                'status' => 404,
+                'monitors' => "No monitors"
+            ], 404);
         }
     }
 
@@ -35,9 +36,9 @@ class MonitorController extends Controller
             'size' => 'required|integer',
             'refresh_rate' => 'required|integer',
             'curved' => 'required|boolean',
-            'price' => 'required|boolean',
+            'price' => 'required|numeric',
+            'description' => 'string',
             'manufacturer_id' => 'required|integer',
-
         ]);
 
         if ($validator->fails()) {
@@ -56,32 +57,59 @@ class MonitorController extends Controller
                 'description' => $request->description
             ]);
 
+            $monitor->save();
+
+            $component = Component::create([
+                'model' => $request->model,
+                'size' => $request->size,
+                'refresh_rate' => $request->refresh_rate,
+                'curved' => $request->curved,
+                'price' => $request->price,
+                'manufacturer_id' => $request->manufacturer_id,
+                'description' => $request->description,
+                'productable_id' => $monitor->id,
+                'productable_type' => Mouse::class,
+            ]);
+
+            $monitor->component()->save($component);
+
             if ($monitor) {
-                return response()->json(['message' => 'monitor created successfully'], 200);
+                return response()->json([
+                    'status' => 200,
+                    'message' => 'Monitor created successfully'
+                ], 200);
             } else {
-                return response()->json(['message' => 'monitor not created!'], 500);
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Monitor not created!'
+                ], 500);
             }
         }
     }
 
     public function getById($id)
     {
-        $monitor = Monitor::find($id);
+        $monitor = Monitor::with('component')->find($id);
         if ($monitor) {
             return response()->json([$monitor], 200);
         } else {
-            return response()->json(['message' => 'monitor not found!'], 500);
+            return response()->json([
+                'status' => 404,
+                'message' => "No monitor found"
+            ], 404);
         }
     }
 
     public function update(Request $request, int $id)
     {
         $validator = Validator::make($request->all(), [
-            'model' => 'sometimes|required|string',
-            'dpi' => 'sometimes|required|integer',
-            'rgb' => 'sometimes|required|boolean',
-            'manufacturer_id' => 'sometimes|required|integer',
-            'description' => 'string'
+            'model' => 'required|string',
+            'size' => 'required|integer',
+            'refresh_rate' => 'required|integer',
+            'curved' => 'required|boolean',
+            'price' => 'required|numeric',
+            'description' => 'string',
+            'manufacturer_id' => 'required|integer',
         ]);
 
         if ($validator->fails()) {
@@ -101,7 +129,9 @@ class MonitorController extends Controller
                     'curved' => $request->curved,
                     'price' => $request->price,
                     'manufacturer_id' => $request->manufacturer_id,
-                    'description' => $request->description
+                    'description' => $request->description,
+                    'productable_id' => $monitor->id,
+                    'productable_type' => Mouse::class,
                 ]);
                 return response()->json(['message' => 'monitor updated successfully'], 200);
             } else {
