ed0dac35e860c41a1e7748e3168ce737075105a9
Martin Friščić <martin.friscic6@gmail.com>
2023-08-30 12:26:42 +0200

    [FRONTEND] Add motherboards on homepage

--- frontend/vue/src/views/Homepage.vue
+++ frontend/vue/src/views/Homepage.vue
@@ -14,6 +14,17 @@
       <PCPartCard v-for="(cpu, index) in cpus" :key="index" :component="cpu" />
       <!--if i pass props that dont exist, nothing will render!!-->
     </div>
+
+    <h1 style="color: red">Matične ploče</h1>
+
+    <div class="gpus-wrapper">
+      <PCPartCard
+        v-for="(motherboard, index) in mbs"
+        :key="index"
+        :component="motherboard"
+      />
+      <!--if i pass props that dont exist, nothing will render!!-->
+    </div>
   </div>
 </template>
 
@@ -22,10 +33,12 @@ import Hero from "../components/Hero.vue";
 import PCPartCard from "../components/PCPartCard.vue";
 import { ref, onMounted } from "vue";
 
-import { getAllGraphicCards, getAllCPUs } from "../api/api";
+import { getAllGraphicCards, getAllCPUs, getAllMotherboards } from "../api/api";
 const gpus = ref([]);
 
 const cpus = ref([]);
+
+const mbs = ref([]);
 
 onMounted(async () => {
   const graphicCards = await getAllGraphicCards();
@@ -35,6 +48,10 @@ onMounted(async () => {
   const procesors = await getAllCPUs();
   cpus.value = procesors.cpus;
   console.log("Cpus", cpus.value);
+
+  const motherboards = await getAllMotherboards();
+  mbs.value = motherboards.motherboards;
+  console.log("Motherboards", mbs.value);
 });
 </script>
 
