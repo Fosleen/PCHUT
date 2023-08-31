eb5e5ed51003549259d182435d95c8b9a458e7e7
Ana Skarica <ana.skarica.as@gmail.com>
2023-08-31 22:53:04 +0200

    [BACKEND] add post endpoint for orders with products and quantity of each product

--- backend/pc-hut/app/Http/Controllers/OrderController.php
+++ backend/pc-hut/app/Http/Controllers/OrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Order;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class OrderController extends Controller
 {
@@ -46,6 +47,42 @@ class OrderController extends Controller
                 'status' => 404,
                 'message' => "No order"
             ], 404);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'user_id' => 'required|integer',
+            'component_id' => 'required|array',
+            'component_id.*' => 'required|integer',
+            'quantity' => 'required|array',
+            'quantity.*' => 'required|integer',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 422,
+                'errors' => $validator->messages(),
+            ], 422);
+        } else {
+            $order = Order::create([
+                'id' => $request->order_id,
+                'user_id' => $request->user_id,
+            ]);
+
+            foreach ($request->component_id as $index => $component_id) {
+                $quantity = $request->quantity[$index];
+                $order->component()->attach([$component_id => ['quantity' => $quantity]]);
+            }
+
+            $order->save();
+
+            if ($order) {
+                return response()->json(['message' => 'Order created successfully'], 200);
+            } else {
+                return response()->json(['message' => 'Order not created!'], 500);
+            }
         }
     }
 }
