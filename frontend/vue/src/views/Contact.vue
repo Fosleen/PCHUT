61b48786b70e227eec52078eb84c5c0ad9a402eb
Martin Friščić <martin.friscic6@gmail.com>
2023-09-07 13:12:41 +0200

    [FRONTEND] Finish contact page desktop design

--- frontend/vue/src/views/Contact.vue
+++ frontend/vue/src/views/Contact.vue
@@ -1,7 +1,18 @@
 <template>
   <div class="contact-page-wrapper">
+    <img src="../assets/cpu-grey.png" alt="" class="contact-page-grey-cpu" />
+    <img src="../assets/gpu-grey.png" alt="" class="contact-page-grey-gpu" />
+    <img
+      src="../assets/keyboard-grey.png"
+      alt=""
+      class="contact-page-grey-keyboard"
+    />
+
     <div class="contact-form-wrapper">
-      <h2>Kontakt</h2>
+      <div class="contact-form-text-wrapper">
+        <h2>Imate neko pitanje?</h2>
+        <h4>Pošaljite nam poruku i mi ćemo odgovoriti u najkraćem roku</h4>
+      </div>
       <form id="contact-form">
         <InputField
           placeholder="Ime *"
@@ -45,23 +56,34 @@ import Textarea from "../components/Textarea.vue";
   display: flex;
   align-items: center;
   flex-direction: column;
+  padding-top: 48px;
+  padding-bottom: 48px;
+  position: relative;
+  background-color: #eee3ff;
+
+  img {
+    position: absolute;
+  }
 
   .contact-form-wrapper {
     z-index: 1;
-    max-width: 480px;
+    max-width: 600px;
     background-color: $white-dark;
-    width: 90%;
+    width: 100%;
     text-align: center;
     border-radius: 8px;
-    padding: 10px;
     box-shadow: 0px 0px 8px $colorPrimary;
-    padding: 16px 8px 32px;
+    padding-left: 48px;
+    padding-right: 48px;
+    padding-top: 16px;
+    padding-bottom: 32px;
 
     h2 {
       font-family: $fontPrimary;
-      text-transform: uppercase;
+      //text-transform: uppercase;
       color: $colorTextPrimary;
-      margin: 32px;
+      margin-top: 32px;
+      margin-bottom: 8px;
     }
 
     form {
@@ -73,5 +95,30 @@ import Textarea from "../components/Textarea.vue";
       gap: 24px;
     }
   }
+}
+
+.contact-form-text-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+}
+
+.contact-page-grey-gpu {
+  left: -20px;
+  height: 300px;
+  transform: rotate(10deg);
+}
+
+.contact-page-grey-cpu {
+  right: -100px;
+  height: 300px;
+  top: -20px;
+  transform: rotate(30deg);
+}
+.contact-page-grey-keyboard {
+  height: 400px;
+  bottom: -80px;
+  left: 350px;
+  transform: rotate(-20deg);
 }
 </style>
