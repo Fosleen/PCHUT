36728ca2effa93b151d7d17a1d58dd1fafb8aab1
Ana Skarica <ana.skarica.as@gmail.com>
2023-09-07 17:34:43 +0200

    [FRONTEND] add cart details responsive UI connect with payment details

--- frontend/vue/src/views/MyCartPage.vue
+++ frontend/vue/src/views/MyCartPage.vue
@@ -44,21 +44,75 @@
         </div>
       </div>
       <div class="my-cart-details-container">
-        nastavi na placanje i ostale stvari
+        <div class="my-cart-details-delivery">
+          <p>Odaberi način dostave:</p>
+          <div class="delivery-type">
+            <input type="radio" value="home-delivery" v-model="deliveryType" />
+            <img src="../assets/home-delivery.png" alt="home-delivery" />
+            <p>GLS dostava (besplatno)</p>
+          </div>
+          <div class="delivery-type">
+            <input type="radio" value="personal" v-model="deliveryType" />
+            <img src="../assets/delivery.png" alt="personal" />
+            <p>Osobno preuzimanje</p>
+          </div>
+        </div>
+        <div class="my-cart-details-promo-code">
+          <p>Unesi promo kod:</p>
+          <input type="text" />
+        </div>
+        <div class="my-cart-details-data">
+          <div class="my-cart-details-data-delivery-date">
+            <p>Očekivana isporuka:</p>
+            <div>
+              <PhCalendarCheck />
+              <p>25. kolovoz - 28. kolovoz</p>
+            </div>
+          </div>
+          <div class="my-cart-details-details-data-prices">
+            <div class="my-cart-details-details-data-prices-row">
+              <p>Osnovica:</p>
+              <p>6076,26 €</p>
+            </div>
+            <div class="my-cart-details-details-data-prices-row">
+              <p>Porez:</p>
+              <p>2025,42 €</p>
+            </div>
+            <div class="my-cart-details-details-data-prices-row">
+              <p>Dostava:</p>
+              <p>0 €</p>
+            </div>
+            <div class="my-cart-details-details-data-prices-row">
+              <p>Ukupan iznos košarice:</p>
+              <p>8010.65 €</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="my-cart-continue">
+        <router-link to="/detalji-placanja">
+          <Button shape="trapezoid" text="Nastavi na plaćanje" />
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 import Button from "../components/Button.vue";
 import ShoppingCartItem from "../components/ShoppingCartItem.vue";
+import { PhCalendarCheck } from "@phosphor-icons/vue";
+
+const deliveryType = ref("home-delivery");
 </script>
 
 <style lang="scss">
 @import "../utils/theme.scss";
 
 .my-cart-wrapper {
+  padding: 32px 0;
+
   .my-cart-container {
     background-color: $colorBgPrimary;
     max-width: 1200px;
@@ -89,10 +143,163 @@ import ShoppingCartItem from "../components/ShoppingCartItem.vue";
         }
       }
     }
+
+    .my-cart-details-container {
+      background-color: $white-dark;
+      border-radius: 16px 16px 0 0;
+      display: flex;
+      flex-direction: column;
+      gap: 32px;
+      padding: 24px;
+      justify-content: space-between;
+
+      p {
+        font-size: 16px;
+        font-weight: 500;
+      }
+
+      p:first-child {
+        font-size: 20px;
+      }
+
+      .my-cart-details-delivery {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+
+        .delivery-type {
+          flex-direction: row;
+          display: flex;
+          width: fit-content;
+          gap: 6px;
+          padding: 8px;
+          align-items: center;
+          border: 2px solid #d9d9d9;
+          border-radius: 16px;
+          min-height: 48px;
+          white-space: pre;
+
+          img {
+            max-width: 40px;
+          }
+        }
+      }
+
+      .my-cart-details-promo-code {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+
+        input {
+          border: none;
+          background-color: $white-dark;
+          border-bottom: 2px solid $colorTextPrimary;
+          border-radius: 0;
+        }
+      }
+
+      .my-cart-details-data {
+        .my-cart-details-data-delivery-date {
+          background-color: $grey-light;
+          border-radius: 100px;
+          text-align: center;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          padding: 12px;
+          gap: 4px;
+
+          > div {
+            display: flex;
+            align-items: center;
+            gap: 8px;
+            font-size: 24px;
+          }
+        }
+        .my-cart-details-details-data-prices {
+          display: flex;
+          flex-direction: column;
+          gap: 8px;
+          margin-top: 24px;
+
+          .my-cart-details-details-data-prices-row {
+            p {
+              font-size: 16px;
+              font-weight: 600;
+            }
+            &:last-of-type {
+              p {
+                font-size: 20px;
+                font-weight: 700;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    .my-cart-continue {
+      border-top: 4px solid $colorPrimary;
+      display: flex;
+      justify-content: end;
+    }
   }
 }
 
 @media screen and ($tablet) {
+  .my-cart-wrapper {
+    padding: 24px;
+
+    .my-cart-container {
+      .my-cart-details-container {
+        p {
+          font-size: 16px;
+          font-weight: 500;
+        }
+
+        p:first-child {
+          font-size: 20px;
+        }
+
+        .my-cart-details-delivery {
+          flex-direction: row;
+          gap: 16px;
+          align-items: center;
+        }
+
+        .my-cart-details-promo-code {
+          width: 300px;
+        }
+
+        .my-cart-details-data {
+          flex-direction: row;
+          display: flex;
+          gap: 48px;
+
+          .my-cart-details-data-delivery-date {
+            width: 300px;
+            height: fit-content;
+          }
+
+          .my-cart-details-details-data-prices {
+            .my-cart-details-details-data-prices-row {
+              p {
+                font-size: 18px;
+                font-weight: 500;
+              }
+
+              &:last-of-type {
+                p {
+                  font-size: 20px;
+                  font-weight: 700;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 }
 
 @media screen and ($desktop) {
@@ -118,6 +325,58 @@ import ShoppingCartItem from "../components/ShoppingCartItem.vue";
 
             p {
               font-size: 28px;
+            }
+          }
+        }
+      }
+
+      .my-cart-details-container {
+        flex-direction: row;
+        padding: 48px;
+
+        .my-cart-details-delivery {
+          flex-direction: column;
+          align-items: start;
+        }
+
+        .my-cart-details-promo-code {
+          width: 300px;
+        }
+
+        .my-cart-details-data {
+          flex-direction: column;
+          gap: 8px;
+          justify-content: end;
+          align-items: end;
+
+          .my-cart-details-data-delivery-date {
+            width: fit-content;
+
+            > div {
+              padding: 0 24px;
+            }
+          }
+
+          .my-cart-details-details-data-prices {
+            .my-cart-details-details-data-prices-row {
+              display: grid;
+              grid-template-columns: 2fr 1fr;
+              gap: 8px;
+              justify-content: center;
+
+              p {
+                font-size: 20px;
+
+                &:first-of-type {
+                  text-align: end;
+                }
+              }
+
+              &:last-of-type {
+                p {
+                  font-size: 24px;
+                }
+              }
             }
           }
         }
