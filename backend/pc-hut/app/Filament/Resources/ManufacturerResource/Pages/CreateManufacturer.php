c140822656e1862437b83015bae83e22d35f1ceb
Martin Friščić <martin.friscic6@gmail.com>
2023-09-24 16:12:19 +0200

    [BACKEND] Add admin dashboard CRUD operations

---
+++ backend/pc-hut/app/Filament/Resources/ManufacturerResource/Pages/CreateManufacturer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ManufacturerResource\Pages;
+
+use App\Filament\Resources\ManufacturerResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateManufacturer extends CreateRecord
+{
+    protected static string $resource = ManufacturerResource::class;
+}
