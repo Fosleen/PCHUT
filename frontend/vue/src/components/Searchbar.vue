b4f8408205c5323051ac8e0e7259eb91bc3f0966
Ana Skarica <ana.skarica.as@gmail.com>
2023-09-27 16:28:38 +0200

    [FRONTEND] Add changes to searchbar UI no image image to search results, border, color

--- frontend/vue/src/components/Searchbar.vue
+++ frontend/vue/src/components/Searchbar.vue
@@ -8,27 +8,36 @@
       id="searchbar"
       name="searchbar"
       class="searchbar"
-      placeholder="Pretraži... "
+      placeholder="Pretraži po modelu... "
       @input="handleInputChange($event.target.value)"
     />
     <div class="search-results" v-if="resultsLength > 0">
-      <div
-        v-for="result in searchResults"
-        :key="result.id"
-        class="search-results-item"
-      >
-        <img :src="result.images[0]" alt="" />
-
-        <p>
-          {{
-            result.product_type_cro +
-            " " +
-            result.manufacturer.name +
-            " " +
-            result.model
-          }}
-        </p>
-        <p>{{ result.price }} &#x20AC;</p>
+      <div v-for="result in searchResults" :key="result.id">
+        <router-link
+          class="search-results-item"
+          :to="`/${getModelName(result.productable_type)}/${result.id}`"
+        >
+          <img
+            :src="
+              result.images[0]
+                ? result.images[0]
+                : [
+                    'https://www.mobismea.com/upload/iblock/2a0/2f5hleoupzrnz9o3b8elnbv82hxfh4ld/No%20Product%20Image%20Available.png',
+                  ]
+            "
+            :alt="result.model"
+          />
+          <p>
+            {{
+              result.product_type_cro +
+              " " +
+              result.manufacturer.name +
+              " " +
+              result.model
+            }}
+          </p>
+          <p class="price">{{ result.price }} €</p>
+        </router-link>
       </div>
     </div>
   </div>
@@ -52,6 +61,11 @@ const handleInputChange = async (phrase) => {
     item.model.toLowerCase().includes(phrase.toLowerCase())
   );
 };
+
+const getModelName = (item) => {
+  const modelName = item.replace("App\\Models\\", "");
+  return modelName;
+};
 </script>
 
 <style lang="scss">
@@ -68,8 +82,8 @@ const handleInputChange = async (phrase) => {
     padding-left: 40px;
     padding-right: 16px;
     background-color: $grey-light;
-    clip-path: polygon(0 0, 100% 0, 100% 100%, 32% 100%);
-    width: 160px;
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 50% 100%);
+    width: 80px;
     display: flex;
     justify-content: center;
     align-items: center;
@@ -85,6 +99,7 @@ const handleInputChange = async (phrase) => {
     border-bottom: 8px solid $colorPrimary;
     border-right: 8px solid $colorPrimary;
     padding-left: 8px;
+    width: 400px;
 
     &:focus {
       outline: none;
@@ -99,40 +114,47 @@ const handleInputChange = async (phrase) => {
 }
 
 .search-results {
+  z-index: 15000;
   position: absolute;
   top: 100%;
-  left: 0;
-  width: 100%;
-  background-color: $colorTextTertiary;
-  border: 1px solid #ccc;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  right: 0;
+  width: 444px;
+  background-color: $colorPrimary;
+  padding: 0 8px 5px 5px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.6);
   z-index: 1000;
-}
+  border-radius: 0 0 0 8px;
 
-.search-results {
-  //background-color: red;
-  z-index: 15000;
-  //position: absolute;
-}
-
-.search-results-item {
-  border-top: 1px solid $colorTextTertiary;
-  border-bottom: 1px solid $colorTextTertiary;
-  gap: 12px;
-
-  display: flex;
-  flex-direction: row;
-  //color: blue;
-  align-items: center;
-
-  &:hover {
-    color: green;
+  & > * {
+    background-color: $colorBgSecondary;
+    border-bottom: 2px solid $colorTextTertiary;
   }
 
-  img {
-    width: 100px;
-    padding: 4px;
-    //border: 1px solid blue;
+  & > :last-of-type {
+    border-top: none;
+    border-bottom: none;
+    border-radius: 0 0 0 4px;
+  }
+
+  .search-results-item {
+    gap: 12px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    p.price {
+      flex: none;
+      margin-right: 8px;
+    }
+
+    &:hover {
+      color: green;
+    }
+
+    img {
+      width: 100px;
+      padding: 4px;
+    }
   }
 }
 </style>
